d0a89eb0ff0a5fabc32850bdbd13bfb885fc1c17
Akporowho Marvelous <solomonmarvel@hotmail.com>
2020-12-12 18:00:13 +0100

    changed to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ Vue.use(Router);
 
 const router = new Router({
   routes,
-  mode: "history",
-  base: "/",
+  mode: "hash",
+  // base: "/",
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
